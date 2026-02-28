f31869809bc1ae7a870d6acbfcafdbb0d82cdf76
Tim de Groot <tim-degroot@live.nl>
2026-02-28 22:45:03 +0100

    Example update

--- scripts/example.bat
+++ scripts/example.bat
@@ -1,15 +1,15 @@
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002134.xml:prod:project_1 --projectversion 0.1 -z +02:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002151.xml:dev:project_2 --projectversion 0.1 -z +02:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002222.xml:prod:project_1 --projectversion 0.1 -z +07:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002257.xml:dev:project_2 --projectversion 0.1 -z +07:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002338.xml:prod:project_1 --projectversion 1.1 --timezone=-05:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002400.xml:dev:project_2 --projectversion 1.2 --timezone=-05:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002431.xml:prod:project_1 --projectversion 1.2 -z +01:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002457.xml:dev:project_2 --projectversion 1.2 -z +01:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002528.xml:prod:project_1 --projectversion 2.0 -z +09:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002549.xml:dev:project_2 --projectversion 2.3 -z +09:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002636.xml:prod:project_1 --projectversion 2.3 -z +00:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002703.xml:dev:project_2 --projectversion 2.3 -z +00:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002739.xml:prod:project_1 --timezone=-08:00
-python -m robotframework_dashboard.main -g -l -o .\atest\resources\outputs\output-20250313-002915.xml:prod:project_1 --projectversion 2.0 -z +05:30
-python -m robotframework_dashboard.main -n robot_dashboard -o .\atest\resources\outputs\output-20250313-003006.xml:prod:project_1 --uselogs -z +02:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002134.xml:prod:project_1 --projectversion 0.1 -z +02:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002151.xml:dev:project_2 --projectversion 0.1 -z +02:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002222.xml:prod:project_1 --projectversion 0.1 -z +07:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002257.xml:dev:project_2 --projectversion 0.1 -z +07:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002338.xml:prod:project_1 --projectversion 1.1 --timezone=-05:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002400.xml:dev:project_2 --projectversion 1.2 --timezone=-05:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002431.xml:prod:project_1 --projectversion 1.2 -z +01:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002457.xml:dev:project_2 --projectversion 1.2 -z +01:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002528.xml:prod:project_1 --projectversion 2.0 -z +09:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002549.xml:dev:project_2 --projectversion 2.3 -z +09:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002636.xml:prod:project_1 --projectversion 2.3 -z +00:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002703.xml:dev:project_2 --projectversion 2.3 -z +00:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002739.xml:prod:project_1 --timezone=-08:00
+robotdashboard -g -l -o .\atest\resources\outputs\output-20250313-002915.xml:prod:project_1 --projectversion 2.0 -z +05:30
+robotdashboard -n robot_dashboard -o .\atest\resources\outputs\output-20250313-003006.xml:prod:project_1 --uselogs -z +02:00
