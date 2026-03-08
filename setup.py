3485cc699c7b9b4a3f446bb13972302b35d30bc3
Tim de Groot <tim-degroot@live.nl>
2026-03-08 21:39:43 +0100

    Bump version to 1.8.0 and update package-lock.json
    - Updated version in version.py and setup.py to 1.8.0.
    - Added "peer": true to several dependencies in package-lock.json.
    - Notable binary change in robot_results.db.

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ extras["all"] = sorted({pkg for group in extras.values() for pkg in group})
 
 setup(
     name="robotframework-dashboard",
-    version="1.7.0",
+    version="1.8.0",
     description="Output processor and dashboard generator for Robot Framework output files",
     long_description="""# 📊 Robot Framework Dashboard
 
