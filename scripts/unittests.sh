9be4cb86d2e7601f6338a70b38c05470e8157746
Tim de Groot <tim-degroot@live.nl>
2026-03-12 00:58:35 +0100

    Add XML coverage report to unit test script

--- scripts/unittests.sh
+++ scripts/unittests.sh
@@ -2,4 +2,4 @@
 set -e
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-COVERAGE_FILE=results/.coverage PYTHONPATH="$SCRIPT_DIR/.." python -m pytest tests/ --cov=robotframework_dashboard --cov-report=term-missing --cov-report=html:results/coverage
+COVERAGE_FILE=results/.coverage PYTHONPATH="$SCRIPT_DIR/.." python -m pytest tests/ --cov=robotframework_dashboard --cov-report=term-missing --cov-report=html:results/coverage --cov-report=xml:coverage.xml
