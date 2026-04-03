03e4f20fa45c426e1c99ce1e1e86de6ba4ad135b
Christian Zahl <cz@dcc-net.de>
2026-04-03 09:05:05 +0200

    test: carve out dockerfile to build test image

--- scripts/create-test-image.sh
+++ scripts/create-test-image.sh
@@ -7,30 +7,11 @@ die() { echo "FATAL: $*"; exit 1; }
 
 docker -v || die "docker seems not being installed"
 
-docker build --tag test-dashboard -f - . <<EOF-EOF
-FROM mcr.microsoft.com/playwright:v1.56.0-jammy
+docker build --tag test-dashboard -f tests/test-image.dockerfile "${@}" .
 
-COPY requirements-test.txt /tmp/requirements-test.txt
-RUN << EOF
-    # Ensure pip is installed
-    apt-get update
-    apt-get install -y python3-pip
-
-    # Install Robot Framework and Python dependencies
-    python3 -m pip install --upgrade pip
-    pip3 install -r /tmp/requirements-test.txt
-
-    # Initialize Browser library
-    rfbrowser init
-EOF
-
-# create a workspace directory, where we mount the repo into
-WORKDIR /robotframework-dashboard
-EOF-EOF
-
-# Then run the container in an interactive mode:
+# To run the container in an interactive mode:
 #   docker run -it --rm --ipc=host -v.:/robotframework-dashboard --user 1000:1000 test-dashboard
-# install the dashboard based on the working directory
+# Within the container install the current code from the working directory
 #   pip install .
 # add the ~/.local/bin to your path
 #   export PATH=$PATH:~/.local/bin
