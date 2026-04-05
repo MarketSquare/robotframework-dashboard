449633f76d8aa58b303c703fd12b3ef7a849ab84
Christian Zahl <cz@dcc-net.de>
2026-04-05 12:02:20 +0200

    test: refactor docker scripts

--- scripts/docker/create-test-image.sh
+++ scripts/docker/create-test-image.sh
@@ -5,12 +5,21 @@
 
 die() { echo "FATAL: $*"; exit 1; }
 
+IMAGE=
+
+[ $# -gt 0 ] || die "Missing argument for container type (robot, python, js) to use"
+[ "$1" = robot ] && IMAGE=test-dashboard-robot
+[ "$1" = python ] && IMAGE=test-dashboard-python
+[ "$1" = js ] && IMAGE=test-dashboard-js
+[ -n "$IMAGE" ] || die "Unknown container type ($1)"
+shift
+
 docker -v || die "docker seems not being installed"
 
-docker build --tag test-dashboard -f tests/test-image.dockerfile "${@}" .
+docker build --tag $IMAGE -f scripts/docker/$IMAGE.dockerfile "${@}" .
 
 # To run the container in an interactive mode:
-#   docker run -it --rm --ipc=host -v.:/robotframework-dashboard --user 1000:1000 test-dashboard
+#   docker run -it --rm --ipc=host -v.:/robotframework-dashboard --user 1000:1000 test-dashboard-robot
 # Within the container install the current code from the working directory
 #   pip install .
 # add the ~/.local/bin to your path
